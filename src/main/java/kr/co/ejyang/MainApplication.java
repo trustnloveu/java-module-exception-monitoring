31b2da3d32f1532747ae7db42398b79a737596ec
ejyang <trustnloveu@gmail.com>
2023-05-24 13:31:27 +0900

    build.gradle resources 설정 제거

--- src/main/java/kr/co/ejyang/MainApplication.java
+++ src/main/java/kr/co/ejyang/MainApplication.java
@@ -1,9 +1,12 @@
 //package kr.co.ejyang;
 //
+//import org.mybatis.spring.annotation.MapperScan;
 //import org.springframework.boot.SpringApplication;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 //
-//@SpringBootApplication
+//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+//@MapperScan(value = {"kr.co.ejyang.*"})
 //public class MainApplication {
 //
 //    public static void main(String[] args) {
