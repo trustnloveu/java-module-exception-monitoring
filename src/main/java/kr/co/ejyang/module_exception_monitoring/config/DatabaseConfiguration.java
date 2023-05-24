62cacd7be8141cf7a7c091272b4445af2fb33c2c
ejyang <trustnloveu@gmail.com>
2023-05-24 10:07:06 +0900

    내부 DB 설정 Rollback

--- src/main/java/kr/co/ejyang/module_exception_monitoring/config/DatabaseConfiguration.java
+++ src/main/java/kr/co/ejyang/module_exception_monitoring/config/DatabaseConfiguration.java
@@ -1,75 +1,75 @@
-package kr.co.ejyang.module_exception_monitoring.config;
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.*;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
-
-@Slf4j
-@Configuration
-@PropertySource("classpath:application-monitoring-${spring.profiles.active}.properties")
-@EnableTransactionManagement
-public class DatabaseConfiguration {
-
-//	@PostConstruct
-	@Primary
-	@Bean
-	@ConfigurationProperties(prefix = "spring.datasource.hikari")
-	public HikariConfig hikariConfig() {
-		HikariConfig hikariConfig = new HikariConfig();
-		return hikariConfig;
-	}
-
-	@Primary
-	@Bean
-	public DataSource dataSource() {
-		HikariConfig hikariConfig = hikariConfig();
-		try {
-			hikariConfig.setJdbcUrl(hikariConfig.getJdbcUrl());
-			hikariConfig.setUsername(hikariConfig.getUsername());
-			hikariConfig.setPassword(hikariConfig.getPassword());
-		} catch (Exception e){
-			e.printStackTrace();
-		}
-		DataSource dataSource = new HikariDataSource(hikariConfig());
-		log.info("datasource : {}", dataSource);
-		return dataSource;
-	}
-	
-	@Autowired
-	private ApplicationContext applicationContext;
-
-	@Primary
-	@Bean
-	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-		sqlSessionFactoryBean.setDataSource(dataSource);
-		sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath*:sql/*_SQL.xml"));
-		sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("config/mybatis-config.xml"));
-		return sqlSessionFactoryBean.getObject();
-	}
-
-	@Primary
-	@Bean
-	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
-		return new SqlSessionTemplate(sqlSessionFactory);
-	}
-
-	@Primary
-	@Bean
-	public PlatformTransactionManager txManager() {
-		return new DataSourceTransactionManager(dataSource());
-	}
-}
+//package kr.co.ejyang.module_exception_monitoring.config;
+//
+//import com.zaxxer.hikari.HikariConfig;
+//import com.zaxxer.hikari.HikariDataSource;
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.ibatis.session.SqlSessionFactory;
+//import org.mybatis.spring.SqlSessionFactoryBean;
+//import org.mybatis.spring.SqlSessionTemplate;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.annotation.*;
+//import org.springframework.core.io.ClassPathResource;
+//import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+//import org.springframework.transaction.PlatformTransactionManager;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
+//
+//import javax.sql.DataSource;
+//
+//@Slf4j
+//@Configuration
+//@PropertySource("classpath:application.properties")
+//@EnableTransactionManagement
+//public class DatabaseConfiguration {
+//
+////	@PostConstruct
+//	@Primary
+//	@Bean
+//	@ConfigurationProperties(prefix = "spring.datasource.hikari")
+//	public HikariConfig hikariConfig() {
+//		HikariConfig hikariConfig = new HikariConfig();
+//		return hikariConfig;
+//	}
+//
+//	@Primary
+//	@Bean
+//	public DataSource dataSource() {
+//		HikariConfig hikariConfig = hikariConfig();
+//		try {
+//			hikariConfig.setJdbcUrl(hikariConfig.getJdbcUrl());
+//			hikariConfig.setUsername(hikariConfig.getUsername());
+//			hikariConfig.setPassword(hikariConfig.getPassword());
+//		} catch (Exception e){
+//			e.printStackTrace();
+//		}
+//		DataSource dataSource = new HikariDataSource(hikariConfig());
+//		log.info("datasource : {}", dataSource);
+//		return dataSource;
+//	}
+//
+//	@Autowired
+//	private ApplicationContext applicationContext;
+//
+//	@Primary
+//	@Bean
+//	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+//		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+//		sqlSessionFactoryBean.setDataSource(dataSource);
+//		sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath*:sql/*_SQL.xml"));
+//		sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("config/mybatis-config.xml"));
+//		return sqlSessionFactoryBean.getObject();
+//	}
+//
+//	@Primary
+//	@Bean
+//	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+//		return new SqlSessionTemplate(sqlSessionFactory);
+//	}
+//
+//	@Primary
+//	@Bean
+//	public PlatformTransactionManager txManager() {
+//		return new DataSourceTransactionManager(dataSource());
+//	}
+//}
--- src/main/java/kr/co/ejyang/module_exception_monitoring/service/MonitoringServiceImpl.java
+++ src/main/java/kr/co/ejyang/module_exception_monitoring/service/MonitoringServiceImpl.java
@@ -2,14 +2,10 @@ package kr.co.ejyang.module_exception_monitoring.service;
 
 import kr.co.ejyang.module_exception_monitoring.dto.MonitoringDto;
 import kr.co.ejyang.module_exception_monitoring.mapper.MonitoringMapper;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Slf4j
-@Service
 public class MonitoringServiceImpl implements MonitoringService {
 
     private final MonitoringMapper monitoringMapper;
@@ -36,34 +32,26 @@ public class MonitoringServiceImpl implements MonitoringService {
      *******************************************************************************************/
     @Override
     public void insertExceptionHistory(String appName, String level, String type, String detail) {
-        try {
-            MonitoringDto.Insert dto = MonitoringDto.Insert.builder()
-                    .appName(appName)
-                    .level(level)
-                    .type(type)
-                    .detail(detail)
-                    .build();
+        MonitoringDto.Insert dto = MonitoringDto.Insert.builder()
+                .appName(appName)
+                .level(level)
+                .type(type)
+                .detail(detail)
+                .build();
 
-            monitoringMapper.insertExceptionHistory(dto);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        monitoringMapper.insertExceptionHistory(dto);
     }
 
     /*******************************************************************************************
      * 에러 갱신 - 알람 발송 유무
      *******************************************************************************************/
     public void updateExceptionHistory(long idx, String alarmYn) {
-        try {
-            MonitoringDto.Update dto = MonitoringDto.Update.builder()
-                    .idx(idx)
-                    .alarmYn(alarmYn)
-                    .build();
+        MonitoringDto.Update dto = MonitoringDto.Update.builder()
+                .idx(idx)
+                .alarmYn(alarmYn)
+                .build();
 
-            monitoringMapper.updateExceptionHistory(dto);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        monitoringMapper.updateExceptionHistory(dto);
 }
 
 
